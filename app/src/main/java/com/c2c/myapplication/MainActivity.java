93c3cd2d583a103abac301fbbbaed17c44f50460
Sahil Singh <singhsittu430@gmail.com>
2020-04-24 23:07:11 +0530

    Symptoms and Precaution Layout created with animation.

--- app/src/main/java/com/c2c/myapplication/MainActivity.java
+++ app/src/main/java/com/c2c/myapplication/MainActivity.java
@@ -2,13 +2,28 @@ package com.c2c.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btnSymptoms);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SymptomsPrecaution.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/c2c/myapplication/SymptomsPrecaution.java
@@ -0,0 +1,14 @@
+package com.c2c.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SymptomsPrecaution extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_symptoms_precaution);
+    }
+}
