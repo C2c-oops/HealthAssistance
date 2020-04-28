6c0418c5424f98285fd9cf6dba6c3c6f31cb8d99
Sahil Singh <singhsittu430@gmail.com>
2020-04-28 19:20:56 +0530

    Live Updates layout created(Bottom sheet dialog, MapView & Counter).

--- app/src/main/java/com/c2c/myapplication/MainActivity.java
+++ app/src/main/java/com/c2c/myapplication/MainActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
+    ImageButton button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +19,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.btnSymptoms);
+        button1 = findViewById(R.id.liveUpdateButton);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), SymptomsPrecaution.class);
+                startActivity(intent);
+            }
+        });
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LiveUpdates.class);
                 startActivity(intent);
             }
         });
